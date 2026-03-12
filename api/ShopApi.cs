b74a281f3aefceb0b3c5c8885fabac36bba2b28b
ursveera <ve2ra3@gmail.com>
2026-03-12 18:29:07 +0530

    new menu

---
+++ api/ShopApi.cs
@@ -0,0 +1,65 @@
+﻿using SharedLib.Models;
+using System.Net.Http.Json;
+
+namespace WASM.Api
+{
+    public class ShopApi : IShopApi
+    {
+        private readonly HttpClient _http;
+
+        private const string BASE = "shop";
+
+        public ShopApi(HttpClient http)
+        {
+            _http = http;
+        }
+
+        public async Task<IEnumerable<ShopItems>> GetAllAsync()
+        {
+            return await _http.GetFromJsonAsync<IEnumerable<ShopItems>>(
+                $"{BASE}"
+            ) ?? Enumerable.Empty<ShopItems>();
+        }
+
+        public async Task<ShopItems?> GetAsync(string id)
+        {
+            return await _http.GetFromJsonAsync<ShopItems>(
+                $"{BASE}/{id}"
+            );
+        }
+
+        public async Task<bool> CreateAsync(ShopItems item)
+        {
+            var response = await _http.PostAsJsonAsync(BASE, item);
+            return response.IsSuccessStatusCode;
+        }
+
+        public async Task<bool> UpdateAsync(string id, ShopItems item)
+        {
+            var response = await _http.PutAsJsonAsync(
+                $"{BASE}/{id}",
+                item
+            );
+
+            return response.IsSuccessStatusCode;
+        }
+
+        public async Task<bool> DeleteAsync(string id)
+        {
+            var response = await _http.DeleteAsync(
+                $"{BASE}/{id}"
+            );
+
+            return response.IsSuccessStatusCode;
+        }
+
+        public async Task<bool> ForceDeleteAsync(string id)
+        {
+            var response = await _http.DeleteAsync(
+                $"{BASE}/force/{id}"
+            );
+
+            return response.IsSuccessStatusCode;
+        }
+    }
+}
